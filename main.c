5a5ac8b3eb75e51ad0b263f6164eb903e4a5c2bb
Robin Walz <robin.walz.wien@gmail.com>
2025-11-04 09:25:00 +0100

    created structs and enums

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+enum priority{
+    highest = 4,
+    high = 3,
+    normal = 2,
+    low = 1,
+    lowest = 0
+}typedef priority;
+
+struct entry{
+    priority prio;
+    char message[32];
+}typedef entry;
+
+struct pqueue{
+    entry entries[10];
+    int isEmtpty;
+}typedef pqueue;
+
 int main()
 {
     printf("Hello world!\n");
